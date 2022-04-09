c3588bb5aa09fd7a8c9ff219b16bb448baa2346d
Anton <klock.anton@gmail.com>
2022-04-09 16:01:06 +0200

    Add CameraView component

--- App.tsx
+++ App.tsx
@@ -1,72 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, Alert, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
-import React, { useState, useEffect } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View, Alert } from "react-native";
+import { Camera } from "expo-camera";
+import React, { useState, useEffect } from "react";
+import CameraView from "./components/CameraView";
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(false);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [useCamera, setUseCamera] = useState(false);
+  const [camButtonText, setCamButtonText] = useState("Scan book");
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  if (!hasPermission) {
-    return <Text>No access to camera</Text>
-  }
   return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-      <Button
-        title="Scan boooks"
-        onPress={() => Alert.alert(`You're now scanning books!`)}
-      />
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        {useCamera ? <CameraView /> : <Text>Press button to use camera.</Text>}
+      </View>
+      <View style={styles.container}>
+        <Button
+          title={camButtonText}
+          onPress={() => {
+            setUseCamera(!useCamera);
+            useCamera
+              ? setCamButtonText("Scan book")
+              : setCamButtonText("Stop scanning");
+          }}
+        />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  camera: {
-    flex: 1,
-    width: 1000,
-  },
-  buttonContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    margin: 20,
-  },
-  button: {
-    flex: 0.1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-    color: 'white',
-  }
 });
